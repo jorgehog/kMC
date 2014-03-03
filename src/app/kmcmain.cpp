c7d5382dde45e00fb20c37970858af021491f636
jorgehog <jorgen_da_mad@hotmail.com>
2014-03-04 00:11:28 +0100

    disassembly checks that kmcd_assert was really optimized to nothing. Ready for merge.

--- src/app/kmcmain.cpp
+++ src/app/kmcmain.cpp
@@ -11,15 +11,17 @@ int main()
 
     Config cfg;
 
-    cfg.readFile("infiles/knowncase.cfg");
+    cfg.readFile("infiles/config.cfg");
 
     const Setting & root = cfg.getRoot();
 
     KMCDebugger_SetFilename("sameSaddleProblem");
     KMCSolver* solver = new KMCSolver(root);
 
+    wall_clock t;
+    t.tic();
     solver->run();
-
+    cout << "Simulation ended after " << t.toc() << " seconds" << endl;
 
 
     return 0;
