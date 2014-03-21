d8184cd508b4443c4c4fb8c7cae8ef50e0e10e20
jorgehog <jorgen_da_mad@hotmail.com>
2014-03-21 12:10:29 +0100

    separated ot particlestates struct to own file

---
+++ src/particlestates.cpp
@@ -0,0 +1,5 @@
+#include "particlestates.h"
+
+ParticleStates::ParticleStates()
+{
+}
---
+++ src/particlestates.h
@@ -0,0 +1,10 @@
+#ifndef PARTICLESTATES_H
+#define PARTICLESTATES_H
+
+class ParticleStates
+{
+public:
+    ParticleStates();
+};
+
+#endif // PARTICLESTATES_H
