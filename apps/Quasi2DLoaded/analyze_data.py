3e6434b3d8703365c6533e8b515557677f3bcac4
jorgehog <jorgen_da_mad@hotmail.com>
2014-09-09 17:25:59 +0200

    updates to script for selecting concentration and esmax values

--- apps/Quasi2DLoaded/analyze_data.py
+++ apps/Quasi2DLoaded/analyze_data.py
@@ -7,39 +7,52 @@ f = h5py.File(sys.argv[1], 'r')
 
 alldata = {}
 
+c0 = 0.9
+em0 = 2
+
 n = len(f.keys())
 k = 0
 for l, run in f.items():
     for potential, data in run.items():
-        t = float(re.findall("beta\_(\d+\.?\d*)", potential)[0])
+
+        eb, t, em, c = [float(re.findall("%s\_(\d+\.?\d*)" % ID, potential)[0]) for ID in ["Eb", "beta", "EsMax", "concentration"]]
+
+        if c != c0 or em != em0*eb:
+            continue
 
         if t not in alldata.keys():
             alldata[t] = {}
+        if em not in alldata[t].keys():
+            alldata[t][em] = {}
+        if c not in alldata[t][em].keys():
+            alldata[t][em][c] = {}
 
-        alldata[t][l] = {}
+        alldata[t][em][c][l] = {}
 
         for i, name in enumerate(data["ignisEventDescriptions"][0]):
-            alldata[t][l][str(name).split("@")[0]] = data["ignisData"][i, :]
+            alldata[t][em][c][l][str(name).split("@")[0]] = data["ignisData"][i, :]
+        alldata[t][em][c][l]["name"] = potential
 
     print k+1, " / ", n, "complete."
     k += 1
 
-for t, lengths in alldata.items():
-    figure()
 
-    title("beta = %g" % t)
+for t, rest in alldata.items():
+    for em, rest2 in rest.items():
+        for c, lengths in rest2.items():
+                figure()
 
-    hold("on")
-    for length, data in lengths.items():
-        loglog(data["Time"], data["heightRMS"], label=length)
+                hold("on")
+                for length, data in lengths.items():
+                    loglog(data["Time"], data["heightRMS"], label=length)
 
-    legend(loc=2)
-    xlabel("t [s * R_0]")
-    ylabel("RMS(h)")
-    draw()
-    savefig("t_vs_hrms_beta" + str(t) + ".png")
+                    title(data["name"])
 
-show()
+                legend(loc=2)
+                xlabel("t [s * R_0]")
+                ylabel("RMS(h)")
+                draw()
+                savefig(data["name"] + ".png")
 
 f.close()
 
